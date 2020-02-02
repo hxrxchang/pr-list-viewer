d7fd342cef55fe6b9a510d729c62599b633eb4fc
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-02-03 00:17:30 +0900

    I understood hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
-const Container = styled.div`
-  .App {
-    text-align: center;
-  }
-
-  .App-logo {
-    height: 40vmin;
-    pointer-events: none;
-  }
-
-  @media (prefers-reduced-motion: no-preference) {
-    .App-logo {
-      animation: App-logo-spin infinite 20s linear;
-    }
-  }
-
-  .App-header {
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-  }
-
-  .App-link {
-    color: #61dafb;
-  }
-
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-`;
+const Container = styled.div``;
 
 const App: React.FC<{}> = () => {
+  const [url] = useState<string>('http://localhost:3000/');
+  const [count, setCount] = useState<number>(0);
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+
+  const fetchTasks = () => {
+    axios.get(url).then(response => {
+      const tasks: string[] = response.data;
+      setTasks(tasks);
+    });
+  };
+
   return (
-    <>
-      <Container>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </Container>
-    </>
+    <Container>
+      {url}
+      <div>
+        {count}
+        <button
+          onClick={() => {
+            setCount(count + 1);
+            fetchTasks();
+          }}
+        >
+          click
+        </button>
+      </div>
+      <div>
+        {tasks.map(task => (
+          <p key={task}>{task}</p>
+        ))}
+      </div>
+    </Container>
   );
 };
 
