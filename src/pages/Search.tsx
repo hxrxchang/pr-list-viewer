33b02123c31baf11b83b8e1b7ca3311a8ae0ff53
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-02-06 00:39:25 +0900

    fetch data from github

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Axios } from 'axios-observable';
+import { map } from 'rxjs/operators';
+import { useTokenQuery as fetchToken } from '../queries/token-query';
+
+const baseUrl = 'https://api.github.com';
+
+const fetchPullRequests = async (
+  teamName: string,
+  reposotoryName: string,
+  baseBranch: string
+) => {
+  const url = `${baseUrl}/repos/${teamName}/${reposotoryName}/pulls?base=${baseBranch}`;
+  return Axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${fetchToken()}`
+    }
+  })
+    .pipe(map(respnse => respnse.data))
+    .toPromise();
+};
 
 const SearchPage: React.FC<{}> = () => {
+  useEffect(() => {
+    fetchPullRequests(
+      'hxrxchang',
+      'rails-spa-cookie-session-sample',
+      'master'
+    ).then(res => console.log(res));
+  }, []);
   return <h1>SearchPage workd</h1>;
 };
 
