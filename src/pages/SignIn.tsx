e17dbddae96325d85cdb13134dffab92117b41a1
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-02-06 00:45:32 +0900

    remove

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 import { useForm } from 'react-hook-form';
 
 type FormData = {
@@ -14,51 +13,19 @@ const Container = styled.div`
 `;
 
 const SignInPage: React.FC<{}> = () => {
-  const [url] = useState<string>('http://localhost:3000/');
-  const [count, setCount] = useState<number>(0);
-  const [tasks, setTasks] = useState<string[]>([]);
   const { register, handleSubmit, errors } = useForm<FormData>();
   const onSubmit = handleSubmit(({ token }) => {
     localStorage.setItem('token', token);
   });
 
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  }, [count]);
-
-  const fetchTasks = () => {
-    axios.get(url).then(response => {
-      const tasks: string[] = response.data;
-      setTasks(tasks);
-    });
-  };
-
   return (
     <Container>
-      {url}
-      <div>
-        {count}
-        <button
-          onClick={() => {
-            setCount(count + 1);
-            fetchTasks();
-          }}
-        >
-          click
-        </button>
-      </div>
       <div>
         <form onSubmit={onSubmit}>
           <input name="token" ref={register({ required: true })} />
           <button type="submit">submit</button>
           {errors.token && <p className="error-message">token is required</p>}
         </form>
-      </div>
-
-      <div>
-        {tasks.map(task => (
-          <p key={task}>{task}</p>
-        ))}
       </div>
     </Container>
   );
